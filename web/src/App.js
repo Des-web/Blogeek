ebc8047aef258e33f219d0ce86cdc8b301286f38
PeterMatthew <pedro_mateus_12@hotmail.com>
2021-08-06 00:38:28 -0300

    add routes and Home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
