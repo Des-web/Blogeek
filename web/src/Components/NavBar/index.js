385e17943ca8441f9f43d5996695bf5dd0ad0076
Cicero Lino <cicerolinoeneto@gmail.com>
2021-08-08 20:31:46 -0300

    Add NavBar component

---
+++ web/src/Components/NavBar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function NavBar(){
+    return(
+        <header>
+            <div class="logo">
+                <h4>The Nav</h4>
+            </div>
+            <ul class="nav-links">
+                <li>
+                    <a href="x">Login</a>
+                </li>
+                <li>
+                    <a href="x">Posts</a>
+                </li>
+                <li>
+                    <a href="x">About</a>
+                </li>
+                <li>
+                    <a href="x">Contato</a>
+                </li>
+            </ul>
+        </header>
+    )
+}
