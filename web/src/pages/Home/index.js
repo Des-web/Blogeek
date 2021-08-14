a5d2b414213952ca07719dd1865a1eeac0265afb
ErickMacena <erickmacena2@gmail.com>
2021-08-14 18:18:29 -0300

    About page and MemberProject Component added

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import NavBar from '../../Components/NavBar';
 import Member from '../../Components/Member';
+
 import imgMember1 from '../../assets/erick-img.jpeg'
 
 function Home() {
   return (
     <div>
       <NavBar/>
-      <br></br>
-      <h1>Hello, World!</h1>
       <Member src={imgMember1} name="Erick Macena" description="Programador Front-End"/>
     </div>
   );
--- web/src/routes.js
+++ web/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import About from './pages/About';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/" component={About} />
         <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
